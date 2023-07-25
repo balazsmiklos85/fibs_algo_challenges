9cf4d9dd65c413fc862f53c9584b90def469ea7d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-07-25 18:35:28 +0000

    more readability with OOP

--- challenge_0728_prototype/main.rb
+++ challenge_0728_prototype/main.rb
@@ -1,17 +1,33 @@
 # frozen_string_literal: true
 
-def largest_rectangle_under_skyline(buildings)
-  largest_rectangle = 0
-  buildings.min.upto(buildings.max) do |height|
-    rectangle_size = 0
-    buildings.each do |building|
-      rectangle_size = building >= height ? rectangle + height : 0
-      largest_rectangle = rectangle if rectangle > largest_rectangle
+class LargestRectangleBuilder
+  def initialize(buildings)
+    @buildings = buildings
+    @heights = @buildings.sort
+                         .uniq
+    @largest_rectangle = 0
+  end
+
+  def build
+    @heights.each { |height| build_for height }
+    @largest_rectangle
+  end
+
+  private
+
+  def build_for(height)
+    rectangle = 0
+    @buildings.each do |building|
+      rectangle = extend_or_close(building, height, rectangle)
+      @largest_rectangle = rectangle if rectangle > @largest_rectangle
     end
   end
-  largest_rectangle
+
+  def extend_or_close(building, height, rectangle)
+    building >= height ? rectangle + height : 0
+  end
 end
 
-input = [1, 3, 3, 2, 4, 1, 5, 3, 2]
+buildings = [1, 3, 3, 2, 4, 1, 5, 3, 2]
 
-puts largest_rectangle_under_skyline(input)
+puts LargestRectangleBuilder.new(buildings).build
