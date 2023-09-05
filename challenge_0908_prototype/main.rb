c86780391b0897ec78c84a7389a3f70e3bd12f6c
Miklos Balazs <balazsmiklos85+git@gmail.com>
2023-09-05 15:23:45 +0000

    more readable code

--- challenge_0908_prototype/main.rb
+++ challenge_0908_prototype/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-class Intern
+class Assignment
   def initialize(id, team)
     @id = id
     @team = team
@@ -17,24 +17,38 @@ class Intern
   end
 end
 
-def print_result(interns)
-  puts "[\n  #{interns.join(",\n  ")}\n]"
+class Assigner
+  def initialize(team_preferences, intern_preferences)
+    @team_preferences = team_preferences
+    @intern_preferences = intern_preferences
+  end
+
+  def solve_with_brute_force
+    team_count = @intern_preferences.length - 1
+    team_indices = Array(0..team_count)
+    team_assigments = team_indices.permutation(3)
+    team_assigments.map { |raw_assignments| to_assignments raw_assignments }
+                   .min_by { |assignments| calculate_score assignments }
+  end
+
+  private
+
+  def calculate_score(assignments)
+    scores = assignments.map do |assignment|
+      assignment.score @team_preferences, @intern_preferences
+    end
+    scores.reduce(:+)
+  end
+
+  def to_assignments(team_assignment)
+    team_assignment.map.with_index do |team, intern|
+      Assignment.new intern, team
+    end
+  end
 end
 
-def solve_with_brute_force(team_preferences, intern_preferences)
-  team_count = intern_preferences.length - 1
-  Array(0..team_count).permutation(3)
-                      .map do |team_assignment|
-                        team_assignment.map.with_index do |team, intern|
-                          Intern.new intern, team
-                        end
-                      end
-                      .min_by do |interns|
-                        interns.map do |intern|
-                          intern.score team_preferences, intern_preferences
-                        end
-                               .reduce(:+)
-                      end
+def print_result(interns)
+  puts "[\n  #{interns.join(",\n  ")}\n]"
 end
 
 interns_input = [
@@ -49,5 +63,6 @@ teams_input = [
   [0, 2, 1]
 ]
 
-interns = solve_with_brute_force interns_input, teams_input
+interns = Assigner.new(interns_input, teams_input)
+                  .solve_with_brute_force
 print_result interns
